896ca2d0b43009eda0f5c7e42f129e25b5f3b6c9
Joey Cozza <joeycozza@gmail.com>
2017-09-28 20:18:26 -0600

    adding fzf and ag to the macSetup script

--- macSetup.sh
+++ macSetup.sh
@@ -6,6 +6,9 @@ cp ~/Documents/configFiles/zshrc ~/.zshrc
 cp ~/Documents/configFiles/gitConfig ~/.gitconfig
 source ~/.zshrc
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+brew install the_silver_searcher
 brew install neovim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
