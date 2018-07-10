3ce6a9dcc64f7325875d35a3855246fc17d41962
Joey Cozza <joeycozza@gmail.com>
2018-07-10 15:09:01 -0600

    fixing my macSetup script

--- macSetup.sh
+++ macSetup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+ln -s "$HOME/Documents/configFiles/zshrc" "$HOME/.zshrc"
+source "$HOME/.zshrc"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -s "$CONFIG_FILES_PATH/vimrc" "$HOME/vimrc"
-ln -s "$CONFIG_FILES_PATH/vim" "$HOME/vim"
-ln -s "$CONFIG_FILES_PATH/zshrc" "$HOME/zshrc"
-ln -s "$CONFIG_FILES_PATH/gitConfig" "$HOME/gitconfig"
-ln -s "$CONFIG_FILES_PATH/gitignore_global" "$HOME/gitignore_global"
-ln -s "$CONFIG_FILES_PATH/vintrc.yaml" "$HOME/vintrc.yaml"
+ln -s "$CONFIG_FILES_PATH/vimrc" "$HOME/.vimrc"
+ln -s "$CONFIG_FILES_PATH/vim" "$HOME/.vim"
+ln -s "$CONFIG_FILES_PATH/gitConfig" "$HOME/.gitconfig"
+ln -s "$CONFIG_FILES_PATH/gitignore_global" "$HOME/.gitignore_global"
+ln -s "$CONFIG_FILES_PATH/vintrc.yaml" "$HOME/.vintrc.yaml"
 ln -s "$CONFIG_FILES_PATH/eslintrc.js" "$HOME/.eslintrc.js"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install fzf
@@ -21,10 +22,10 @@ brew install hadolint
 brew install luajit
 # grip is for markdown preview github option
 brew install grip
-source "$HOME/zshrc"
-ln -s "$HOME/vimrc" "$HOME/config/nvim/init.vim"
+source "$HOME/.zshrc"
+ln -s "$HOME/.vimrc" "$HOME/config/nvim/init.vim"
 curl -fLo "$HOME/local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-source "$HOME/zshrc"
+source "$HOME/.zshrc"
 sudo pip3 install neovim
 sudo pip3 install vim-vint
 sudo pip3 install neovim-remote
@@ -35,7 +36,7 @@ nvim --headless +UpdateRemotePlugins +qa
 curl https://github.com/tonsky/FiraCode/releases/download/1.204/FiraCode_1.204.zip -o "$HOME/Downloads/FiraCode_1.204.zip"
 curl https://iterm2.com/downloads/stable/iTerm2-3_1_5.zip -o "$HOME/Downloads/iterm2Beta.zip"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
-source "$HOME/zshrc"
+source "$HOME/.zshrc"
 ln -s "$CONFIG_FILES_PATH/default-packages" "$NVM_DIR/default-packages"
 nvm install node
 echo ""
