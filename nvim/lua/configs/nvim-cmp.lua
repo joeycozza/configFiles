212927eb033cf55b194e856b851ef7b44b5bf758
Joey Cozza <joeycozza@gmail.com>
2022-10-02 00:28:24 -0600

    c-y instead of enter to use completion

--- nvim/lua/configs/nvim-cmp.lua
+++ nvim/lua/configs/nvim-cmp.lua
@@ -12,7 +12,7 @@ local nvimCmpConfig = {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+    ['<C-y>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
   }),
   window = {
     -- completion = cmp.config.window.bordered(),
