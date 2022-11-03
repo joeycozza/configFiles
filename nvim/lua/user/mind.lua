8bb65b3c24ec77b2f5548864aa5a4d4af9d9ca2e
Joey Cozza <joeycozza@gmail.com>
2022-11-02 22:16:45 -0600

    dad keymap to close Mind

--- nvim/lua/user/mind.lua
+++ nvim/lua/user/mind.lua
@@ -2,4 +2,5 @@ require('mind').setup()
 local keymap = require('../utils').keymap
 
 keymap('n', 'mom', ':MindOpenMain<CR>')
+keymap('n', 'dad', ':MindClose<CR>')
 keymap('n', 'mop', ':MindOpenProject<CR>')
