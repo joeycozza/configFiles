428efc5af81bb380aab8db508f455db7336e8b2b
Joey Cozza <joeycozza@gmail.com>
2023-01-30 22:11:34 -0700

    bufferline moved to lazy plugin style

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,25 +1,62 @@
+-- LuaFormatter off
 return {
   'navarasu/onedark.nvim',
-  'akinsho/bufferline.nvim', 'ryanoasis/vim-devicons',
-  'kyazdani42/nvim-web-devicons', 'othree/yajs.vim', 'moll/vim-node',
-  'styled-components/vim-styled-components', 'leafgarland/typescript-vim',
+  'ryanoasis/vim-devicons',
+  'kyazdani42/nvim-web-devicons',
+  'othree/yajs.vim',
+  'moll/vim-node',
+  'styled-components/vim-styled-components',
+  'leafgarland/typescript-vim',
+  'MaxMEllon/vim-jsx-pretty',
+  'alvan/vim-closetag',
+  'jxnblk/vim-mdx-js',
+  'airblade/vim-gitgutter',
+  'tpope/vim-fugitive',
+  'MunifTanjim/nui.nvim',
+  'phaazon/mind.nvim',
+  'SirVer/ultisnips',
+  'tpope/vim-surround',
+  'tpope/vim-repeat',
+  'tpope/vim-commentary',
+  'joeycozza/vim-obsession',
+  'wellle/targets.vim',
+  'yuttie/comfortable-motion.vim',
+  'ggandor/leap.nvim',
+  'ggandor/flit.nvim',
+  {
+    'JamshedVesuna/vim-markdown-preview',
+    ft = 'markdown'
+  },
+  'nvim-lua/plenary.nvim',
+  'nvim-telescope/telescope.nvim',
+  'nvim-telescope/telescope-fzf-native.nvim',
+  { 'numToStr/FTerm.nvim', cmd = 'FTerm' },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'David-Kunz/treesitter-unit'
+    }
+  },
+  'pedrohdz/vim-yaml-folds',
+  'dstein64/vim-startuptime',
+  'David-Kunz/jester',
 
-  'MaxMEllon/vim-jsx-pretty', 'alvan/vim-closetag', 'jxnblk/vim-mdx-js', 'airblade/vim-gitgutter',
-  'tpope/vim-fugitive', 'MunifTanjim/nui.nvim', 'phaazon/mind.nvim', 'SirVer/ultisnips',
-  'tpope/vim-surround', 'tpope/vim-repeat', 'tpope/vim-commentary', 'joeycozza/vim-obsession',
-  'wellle/targets.vim', 'yuttie/comfortable-motion.vim', 'ggandor/leap.nvim', 'ggandor/flit.nvim',
-  'JamshedVesuna/vim-markdown-preview', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim',
-  'nvim-telescope/telescope-fzf-native.nvim', {'numToStr/FTerm.nvim', cmd = 'FTerm'},
+  'neovim/nvim-lspconfig',
+  'onsails/lspkind-nvim',
+  'jose-elias-alvarez/null-ls.nvim',
 
-  'nvim-treesitter/nvim-treesitter', 'David-Kunz/treesitter-unit',
-  'nvim-treesitter/nvim-treesitter-textobjects', 'pedrohdz/vim-yaml-folds',
-  'dstein64/vim-startuptime', 'David-Kunz/jester',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-nvim-lua',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/nvim-cmp',
+  'quangnguyen30192/cmp-nvim-ultisnips',
+  'smjonas/inc-rename.nvim',
 
-  'neovim/nvim-lspconfig', 'onsails/lspkind-nvim', 'jose-elias-alvarez/null-ls.nvim',
-
-  'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-nvim-lua', 'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/nvim-cmp', 'quangnguyen30192/cmp-nvim-ultisnips', 'smjonas/inc-rename.nvim',
-
-  'lewis6991/impatient.nvim', 'echasnovski/mini.nvim', 'b0o/incline.nvim',
+  'lewis6991/impatient.nvim',
+  'echasnovski/mini.nvim',
+  'b0o/incline.nvim',
   'zegervdv/nrpattern.nvim'
 }
+-- LuaFormatter on
