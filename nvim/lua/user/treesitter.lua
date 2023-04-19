149f14a544a1be79234f4f29d33918e04873727f
Joey Cozza <joeycozza@gmail.com>
2023-04-18 18:11:11 -0600

    add vimdoc to treesitter

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -5,7 +5,7 @@ treesitterConfigs.setup({
   ensure_installed = {
     'typescript', 'javascript', 'lua', 'bash', 'vim', 'comment', 'css', 'go', 'graphql', 'html',
     'http', 'java', 'jsdoc', 'json', 'json5', 'make', 'markdown', 'markdown_inline', 'ocaml',
-    'python', 'regex', 'yaml'
+    'python', 'regex', 'yaml', 'vimdoc'
   },
   highlight = { enable = true, additional_vim_regex_highlighting = false },
   incremental_selection = {
