2269b97f6bd665bde4e1d1cf33cfd66d82e0865e
Joey Cozza <joeycozza@gmail.com>
2023-10-31 15:11:31 -0600

    setup custom snippets in SnipMate format

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -2,6 +2,22 @@ local cmp = require("cmp")
 return {
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "L3MON4D3/LuaSnip",
+        config = function()
+          -- this isn't working, I can't highlight text, and press Tab, nothing happens afterwards
+          -- local ls = require("luasnip")
+          -- ls.setup({ store_selection_keys = "<Tab>" })
+          local fromSnip = require("luasnip.loaders.from_snipmate")
+          fromSnip.lazy_load({ paths = "./lua/snippets" })
+        end,
+        dependencies = {
+          "rafamadriz/friendly-snippets",
+          "saadparwaiz1/cmp_luasnip",
+        },
+      },
+    },
     opts = {
       sources = cmp.config.sources({
         { name = "luasnip" },
