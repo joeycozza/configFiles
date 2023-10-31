70b88c9ca3f04a74b8d033f68e6530a8499c0cda
Joey Cozza <joeycozza@gmail.com>
2023-10-31 16:00:58 -0600

    tab in visual mode will capture the text ready for snippets

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -6,9 +6,8 @@ return {
       {
         "L3MON4D3/LuaSnip",
         config = function()
-          -- this isn't working, I can't highlight text, and press Tab, nothing happens afterwards
-          -- local ls = require("luasnip")
-          -- ls.setup({ store_selection_keys = "<Tab>" })
+          local ls = require("luasnip")
+          ls.config.setup({ store_selection_keys = "<Tab>" })
           local fromSnip = require("luasnip.loaders.from_snipmate")
           fromSnip.lazy_load({ paths = "./lua/snippets" })
         end,
