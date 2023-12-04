23486e08a89157e67b5fcc1d94aed5f509c494ff
Joey Cozza <joeycozza@gmail.com>
2023-12-04 09:58:43 -0700

    remove markdown plugin, tab to choose files and enter to edit with telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,23 @@
+local trouble = require("trouble.providers.telescope")
+
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local telescope_custom_actions = {}
+
+function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local num_selections = #picker:get_multi_selection()
+  if not num_selections or num_selections <= 1 then
+    actions.add_selection(prompt_bufnr)
+  end
+  actions.send_selected_to_qflist(prompt_bufnr)
+  vim.cmd("cfdo " .. open_cmd)
+end
+
+function telescope_custom_actions.multi_selection_open(prompt_bufnr)
+  telescope_custom_actions._multiopen(prompt_bufnr, "edit")
+end
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -9,6 +29,17 @@ return {
           width = 0.95,
           preview_width = 0.55,
           prompt_position = "top",
+        },
+        mappings = {
+          i = {
+            ["<ESC>"] = actions.close,
+            ["<C-J>"] = actions.move_selection_next,
+            ["<C-K>"] = actions.move_selection_previous,
+            ["<TAB>"] = actions.toggle_selection + actions.move_selection_next,
+            ["<CR>"] = telescope_custom_actions.multi_selection_open,
+            ["<c-t>"] = trouble.open_with_trouble,
+          },
+          n = i,
         },
       },
     },
