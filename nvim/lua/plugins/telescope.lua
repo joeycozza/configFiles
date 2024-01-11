db198fedb17f7fdf7e944705afa6ab6ffd01d87a
Joey Cozza <joeycozza@gmail.com>
2024-01-10 22:28:57 -0700

    remove custom telescope tabbing/open multiple functino

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,22 +1,22 @@
 local trouble = require("trouble.providers.telescope")
 
 local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
-local telescope_custom_actions = {}
+-- local action_state = require("telescope.actions.state")
+-- local telescope_custom_actions = {}
 
-function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
-  local picker = action_state.get_current_picker(prompt_bufnr)
-  local num_selections = #picker:get_multi_selection()
-  if not num_selections or num_selections <= 1 then
-    actions.add_selection(prompt_bufnr)
-  end
-  actions.send_selected_to_qflist(prompt_bufnr)
-  vim.cmd("cfdo " .. open_cmd)
-end
+-- function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
+--   local picker = action_state.get_current_picker(prompt_bufnr)
+--   local num_selections = #picker:get_multi_selection()
+--   if not num_selections or num_selections <= 1 then
+--     actions.add_selection(prompt_bufnr)
+--   end
+--   actions.send_selected_to_qflist(prompt_bufnr)
+--   vim.cmd("cfdo " .. open_cmd)
+-- end
 
-function telescope_custom_actions.multi_selection_open(prompt_bufnr)
-  telescope_custom_actions._multiopen(prompt_bufnr, "edit")
-end
+-- function telescope_custom_actions.multi_selection_open(prompt_bufnr)
+--   telescope_custom_actions._multiopen(prompt_bufnr, "edit")
+-- end
 
 return {
   {
@@ -33,10 +33,10 @@ return {
         mappings = {
           i = {
             ["<ESC>"] = actions.close,
-            ["<C-J>"] = actions.move_selection_next,
-            ["<C-K>"] = actions.move_selection_previous,
-            ["<TAB>"] = actions.toggle_selection + actions.move_selection_next,
-            ["<CR>"] = telescope_custom_actions.multi_selection_open,
+            -- ["<C-J>"] = actions.move_selection_next,
+            -- ["<C-K>"] = actions.move_selection_previous,
+            -- ["<TAB>"] = actions.toggle_selection + actions.move_selection_next,
+            -- ["<CR>"] = telescope_custom_actions.multi_selection_open,
             ["<c-t>"] = trouble.open_with_trouble,
           },
           n = i,
